acab2db963acf8412e413d550051cb9b06019d8b
NicoleElazar <nicole.elazar@gmail.com>
2019-12-14 16:13:15 +0100

    add images

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-console.log('working');
-
 const gameArea = document.getElementById('game-area');
+let picContain;
 let owl;
+let bush;
 
-for (let i = 0; i < 9; i++) {
+
+
+for (let i = 0; i < 6; i++) {
+    picContain = document.createElement('div');
+    gameArea.appendChild(picContain);
+    picContain.classList.add('pic-contain');
+
     owl = document.createElement('div');
-    gameArea.appendChild(owl);
+    picContain.appendChild(owl);
     owl.classList.add('owl-box');
 
+    bush = document.createElement('div');
+    picContain.appendChild(bush);
+    bush.classList.add('bush-box');
 }
